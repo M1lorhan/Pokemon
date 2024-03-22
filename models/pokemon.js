30b0a83e46c323a23641c41824e6825d89829485
Marshall Lorhan <mlorhan@gmail.com>
2024-03-21 21:29:26 -0700

    added Pokemon API functionality

---
+++ models/pokemon.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const pokemonSchema = new mongoose.Schema({
+    height: {
+        type: Number
+    },
+    weight: {
+        type: Number
+    },
+    pokemonId: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    image: String
+})
+
+module.exports = mongoose.model('Pokemon', pokemonSchema)
