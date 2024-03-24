7924a3b024b0f9ec035fff7f9b581632d1a8681a
Marshall Lorhan <mlorhan@gmail.com>
2024-03-24 12:32:22 -0700

    updated find by id

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const Pokemon = require('../models/pokemon')
+const Pokemon = require('../models/Pokemon')
 
 router.get('/', async (req, res) => {
     try {
@@ -15,7 +15,7 @@ router.get('/:pokemonId', async (req, res) => {
     try {
         const { pokemonId } = req.params
         const pokemon = await Pokemon.find({ pokemonId })
-        
+        res.json(pokemon)        
     } catch (error) {
         console.log('error fetching all pokemon', error)
         res.status(500).json({message: 'error fetching all pokemon'})
